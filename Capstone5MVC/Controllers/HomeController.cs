01839203f5c09a078fc0bc5ec5830ed8e1998933
Brandyn Tweedly <tweedly92@gmail.com>
2019-09-04 08:49:55 -0400

    first commit

---
+++ Capstone5MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Capstone5MVC.Models;
+using System.Net.Http;
+
+namespace Capstone5MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Search(int Id)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44374");
+
+            var response = await client.GetAsync($"cars/{Id}.json");
+            var donut = await response.Content.ReadAsAsync<Cars>();
+
+            return View(donut);
+        }
+    }
+}
---
+++ Capstone5MVC/Models/Cars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone5MVC.Models
+{
+
+    //public class Rootobject
+    //{
+    //    public Class1[] Property1 { get; set; }
+    //}
+
+    public class Cars
+    {
+        public int carId { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public string year { get; set; }
+        public string color { get; set; }
+    }
+
+}
